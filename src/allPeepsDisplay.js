b0740435b4ee1e14acfd0d416b6b16cf805bb9fc
Kerri McMahon <kerrimcm25@gmail.com>
2021-06-28 22:00:40 +0100

    Data from peep gets console logged on button click

--- src/allPeepsDisplay.js
+++ src/allPeepsDisplay.js
@@ -4,10 +4,23 @@ const fetchData = () => {
   .then(json => json.forEach((post) => {
     let list = document.createElement("ul");
     let body = document.createElement("p");
+    let button = document.createElement("button");
     body.innerHTML = `${post.user.handle}: ${post.body}`
+    button.innerHTML = "View peep"
+    button.addEventListener('click', () => {displayPeep(post.id)});
+    body.appendChild(button);
     list.appendChild(body);
     document.getElementById('main').appendChild(list);
   }))
 }
 
 fetchData()
+
+const displayPeep = (id) => {
+  fetch(`https://chitter-backend-api-v2.herokuapp.com/peeps/${id}`)
+  .then(res => res.json())
+  .then(json => {
+    console.log(json);
+  })
+}
+  
