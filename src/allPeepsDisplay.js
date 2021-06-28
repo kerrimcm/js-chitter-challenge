e58af00c26e32af1d179d4e6c6d687ea10ed6e88
Kerri McMahon <kerrimcm25@gmail.com>
2021-06-28 22:24:17 +0100

    can view single peeps on their own page

--- src/allPeepsDisplay.js
+++ src/allPeepsDisplay.js
@@ -1,3 +1,6 @@
+const main = document.getElementById('main')
+const single = document.getElementById('single')
+
 const fetchData = () => {
   fetch('https://chitter-backend-api-v2.herokuapp.com/peeps')
   .then(res => res.json())
@@ -10,7 +13,7 @@ const fetchData = () => {
     button.addEventListener('click', () => {displayPeep(post.id)});
     body.appendChild(button);
     list.appendChild(body);
-    document.getElementById('main').appendChild(list);
+    main.appendChild(list);
   }))
 }
 
@@ -19,8 +22,17 @@ fetchData()
 const displayPeep = (id) => {
   fetch(`https://chitter-backend-api-v2.herokuapp.com/peeps/${id}`)
   .then(res => res.json())
-  .then(json => {
-    console.log(json);
+  .then(post => {
+    main.style.display = "none"
+    single.style.display = "block"
+    single.innerHTML = ""
+    let body = document.createElement("p")
+    let button = document.createElement("button");
+    body.innerHTML = `${post.body}`
+    button.innerHTML = "Back to Peeps"
+    button.addEventListener('click', () => {main.style.display = "block", single.style.display = "none"});
+    body.appendChild(button);
+    single.appendChild(body)
   })
 }
   
